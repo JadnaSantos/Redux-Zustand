41675770d7d04b33672fd294b683439e3380b07f
Jadna <jadnajesus7@gmail.com>
2024-08-14 12:23:39 -0300

    feat: add provider from redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./styles/global.css";
 import { Player } from "./pages/Player";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export function App() {
-  return <Player />;
+  return (
+    <Provider store={store}>
+      <Player />;
+    </Provider>
+  );
 }
